12239cf8994942ef711d1b36b2f00aa0911352f9
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-12-14 20:03:08 +0300

    Add discover view function

--- lecturer/views.py
+++ lecturer/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def lecture(request):
     return render(request, 'lecture.html')
+
+
+def discover(request):
+    return render(request, 'discover.html')
--- student/views.py
+++ student/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def student(request):
+
     return render(request, 'student.html')
