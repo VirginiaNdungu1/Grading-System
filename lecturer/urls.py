577e544deac031e916f353e1257237eb9bd9da26
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-12-23 16:04:20 +0300

    lec- urls * dashboard * discover * profile * create new project

--- lecturer/urls.py
+++ lecturer/urls.py
@@ -4,8 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.lecture, name='lecture'),
-    url(r'^discover/$', views.discover, name='discover')
+    url(r'^$', views.lecture, name='lecturer'),
+    url(r'^profiles/edit', views.update_user_profile, name='updateuserprofile'),
+    url(r'^discover/$', views.discover, name='discover'),
+    url(r'^account/(\d+)$', views.account, name='account'),
+    url(r'^project/create', views.create_project, name='create_project')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
