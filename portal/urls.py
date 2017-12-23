49274069789a4cf047a1cdbbf22ef6c5b98c2da9
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-12-23 16:01:13 +0300

    portal - urls Additions * import settings and static * media urls initialisations

--- portal/urls.py
+++ portal/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.contrib.auth import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -33,3 +35,6 @@ urlpatterns = [
     url(r'^lec/', include('lecturer.urls')),
     url(r'^student/', include('student.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
