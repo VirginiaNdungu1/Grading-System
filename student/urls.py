2e2643d759a029971a304f53a9f96d0f4665738e
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-12-23 15:59:46 +0300

    student - urls Additions * profile * edit profile

--- student/urls.py
+++ student/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.student, name='student'),
+    url(r'^profiles/edit', views.update_user_profile, name='updateuserprofile'),
+    url(r'^student_account/(\d+)$', views.account, name='student_account'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
